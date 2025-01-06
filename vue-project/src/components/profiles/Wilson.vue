e466ccd557b5a16549db870a1b15c9fe4b001486
Jasper Fei <j4fei@uwaterloo.ca>
2025-01-06 13:57:43 -0500

    fixed ID

--- vue-project/src/components/profiles/Wilson.vue
+++ vue-project/src/components/profiles/Wilson.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="jasper" class="min-h-screen flex items-center justify-center bg-gray-50 p-8">
+  <section id="wilson" class="min-h-screen flex items-center justify-center bg-gray-50 p-8">
     <div class="max-w-3xl text-center">
       <h2 class="text-3xl font-bold text-black mb-4">Wilson</h2>
     </div>
